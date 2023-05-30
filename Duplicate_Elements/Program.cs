d61a8b7d1ca040a87f3e9d257478b61d419a6c2e
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-05-30 09:56:16 +0530

    OOPP_Duplicate_Elements

---
+++ Duplicate_Elements/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Duplicate_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 1, 2, 4, 4, 5, 5};
+            int count = 0;
+
+            // Loop through each element in the array
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                // Check if the current element is not already counted as a duplicate
+                if (numbers[i] != -1)
+                {
+                    // Loop through the remaining elements
+                    for (int j = i + 1; j < numbers.Length; j++)
+                    {
+                        // If a duplicate is found
+                        if (numbers[i] == numbers[j])
+                        {
+                            // Mark the duplicate element as -1
+                            numbers[j] = -1;
+                            // Increment the count of duplicates
+                            count++;
+                        }
+                    }
+                }
+            }
+
+            // Display the total number of duplicate elements
+            Console.WriteLine("Total number of duplicate elements: " + count);
+        }
+    }
+}
