d8764a2b8b09b2d04de63cf3b2cd90fc8bf8a5af
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-05-31 19:18:12 +0530

    Min_Max_Element

---
+++ Duplicate_Elements/Min_Max_Element.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Duplicate_Elements
+{
+    internal class Min_Max_Element
+    {
+        public static void Min_Max()
+        {
+            Console.Write("Enter the number of elements: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            Console.WriteLine("Enter the elements:");
+
+            // Reading the elements from the user
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Element {i + 1}: ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+
+            // Initializing variables for maximum and minimum elements
+            int max = arr[0];
+            int min = arr[0];
+
+            // Finding the maximum and minimum elements
+            for (int i = 1; i < n; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+
+            // Printing the maximum and minimum elements
+            Console.WriteLine($"Maximum element: {max}");
+            Console.WriteLine($"Minimum element: {min}");
+        }
+    }
+
+}
+    
+
--- Duplicate_Elements/Program.cs
+++ Duplicate_Elements/Program.cs
@@ -8,7 +8,7 @@ namespace Duplicate_Elements
         {
 
             Console.WriteLine("Please Choose any one program from below option");
-            Console.WriteLine("1.Duplicate_Element\n2.Unique_Elements\n3.Count_the_frequency");
+            Console.WriteLine("1.Duplicate_Element\n2.Unique_Elements\n3.Count_the_frequency\n4.Min_Max_Element");
             int option = Convert.ToInt32(Console.ReadLine());
            
             
@@ -25,6 +25,10 @@ namespace Duplicate_Elements
 
                 case 3:
                     Count_the_frequency.Count_frequency();
+                    break;
+
+                case 4:
+                    Min_Max_Element.Min_Max();
                     break;
             }
 
