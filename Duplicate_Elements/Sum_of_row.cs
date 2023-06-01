53db1f954a2e36a9a37cd8f68548590b47bcff71
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-06-01 08:14:18 +0530

    Sum_of_row

---
+++ Duplicate_Elements/Sum_of_row.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Duplicate_Elements
+{
+    internal class Sum_of_row
+    {
+        public static void Sum_row()
+        {
+            // Define a 2D matrix
+            int[,] matrix = {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
+
+            int rows = matrix.GetLength(0); // Get the number of rows in the matrix
+
+            for (int i = 0; i < rows; i++)
+            {
+                int sum = 0; // Variable to store the sum of each row
+
+                // Iterate through each element of the current row
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    sum += matrix[i, j]; // Add the element to the sum
+                }
+
+                Console.WriteLine("Sum of row {0}: {1}", i + 1, sum); // Output the sum of the current row
+            }
+        }
+    }
+}
